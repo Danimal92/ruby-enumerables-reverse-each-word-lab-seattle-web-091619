fb5f3a1ac4b5b925f55e0eeecb4a7de8394e78f9
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-30 09:10:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,7 @@
 def reverse_each_word(array)
     result = []
     tester = array.split(/ /)
-    tester.collect {|word| result << word.reverse}
-    
-    
-  end
-    result.join(' ')
-        
-    return result
-end
+    tester.collect{|word|
+      result << word.reverse}
+    return result.join(' ')
+    end
